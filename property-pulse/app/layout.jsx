5a9b3fc029f988813ffe4dde0a46f54d1e8d9268
engmoha1717 <52564935+engmoha1717@users.noreply.github.com>
2024-04-01 19:07:15 +0300

    second commit

---
+++ property-pulse/app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+    title: 'PropertyPulse | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties',
+  };
+const MainLayout = ({children}) => {
+  return (
+    <html>
+        <body>
+            <div>{children}</div>
+        </body>
+        
+    </html>
+    
+  )
+}
+
+export default MainLayout
